f924c395fcca5de961bef9320e8effc40b03be0d
Hank Hng <hankhng@hotmail.com>
2021-04-25 19:25:08 +0100

    Search box and styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+const api = {
+  key: "e2bde334f3a4275655cedbbd5ab6df1b",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input
+          type="text"
+          className="search-bar"
+          placeholder="Search..."
+          />
+        </div>
+          <div className="location-box">
+            <div className="location">London, UK</div>
+            <div className="date">{dateBuilder(new Date())}</div>
+          </div>
+          <div className="weather-box">
+            <div className="temp">
+              15c
+            </div>
+            <div className="weather">
+              Sunny
+            </div>
+          </div>
+      </main>
     </div>
   );
 }
